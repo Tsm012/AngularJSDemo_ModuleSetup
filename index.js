6e758ec4d598bc722d1e362c504b52e4c86bb29d
Tsm012 <Tsm012@bucknell.edu>
2020-04-27 22:54:19 -0700

    moved oclazyload to next demo

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import angular from "angular";
 import "@uirouter/angularjs";
-import "oclazyload";
 import "./modules/main.module"
 
-let app = angular.module("AngularComponentDemo", ['ui.router', 'oc.lazyLoad', 'main.module'])
+let app = angular.module("AngularComponentDemo", ['ui.router', 'main.module'])
     
 app.config(["$locationProvider", function ($locationProvider) {    
     $locationProvider.html5Mode(true);
